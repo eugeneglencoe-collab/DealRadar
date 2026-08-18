8f85462e7c35249d99348338beefc526343abff3
eugeneglencoe-collab <eugene.glencoe@gmail.com>
2026-08-18 19:01:33 +0200

    fix: add npmrc config and improve jest configuration for React 19 compatibility

--- jest.config.js
+++ jest.config.js
@@ -14,6 +14,11 @@ const customJestConfig = {
     '**/__tests__/**/*.[jt]s?(x)',
     '**/?(*.)+(spec|test).[jt]s?(x)',
   ],
+  testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
+  transformIgnorePatterns: [
+    '/node_modules/',
+    '^.+\\.module\\.(css|sass|scss)$',
+  ],
 }
 
 module.exports = createJestConfig(customJestConfig)
